0e9e5ea13757041242bb8ba9de946ebac61cf6f6
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-01 11:11:53 +0300

    JS

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      codes[index].value = "";
+      setTimeout(() => codes[index + 1].focus(), 10);
+    }
+
+    if (e.key === "Backspace") {
+      codes[index - 1].value = "";
+      setTimeout(() => codes[index - 1].focus(), 10);
+    }
+  });
+});
